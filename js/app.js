b033d14894538102ce86711a6f5e5953fe97bb84
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-07-30 00:55:23 -0500

    new changes

--- js/app.js
+++ js/app.js
@@ -1,21 +1,43 @@
 const listContainer = document.querySelector("[data-lists]");
+const newListForm = document.querySelector("[data-new-list-form]");
+const newListInput = document.querySelector("[data-new-list-input]");
 
-//list TODO
-let lists = ["name", "todo"];
+//list TODO create objec id and name
+// let lists = [{
+//     id: Date.now(),
+//     name: 'name'
+// }, {
+//     id: Date.now(),
+//     name: 'todo'
+// }];
+let lists = [{
+    id: 1,
+    name: 'name'
+}, {
+    id: 2,
+    name: 'todo'
+}];
+
+newListForm.addEventListener('submit', e=> {
+    e.preventDefault();
+}) 
+
 
 function render() {
   //<li class="list-name">Grocery</li>
-  console.log(listContainer);
+  
   clearElement(listContainer);
   lists.forEach((list) => {
     const listElement = document.createElement("li");
+    listElement.dataset.listId = list.id
     listElement.classList.add("list-name");
-    listElement.innerText = list;
+    listElement.innerText = list.name;
     listContainer.appendChild(listElement)
   });
 }
 
 function clearElement(element) {
+    //delete html original
     while(element.firstChild){
         element.removeChild(element.firstChild);
     }
