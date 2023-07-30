4a640d97cb662a9a777194c2d8d70cb2bcdf550c
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-07-30 00:36:32 -0500

    new changes

--- js/app.js
+++ js/app.js
@@ -1,5 +1,25 @@
-const listContainer = document.querySelector('[data-lists]')
+const listContainer = document.querySelector("[data-lists]");
 
-let list = [];
+//list TODO
+let lists = ["name", "todo"];
 
-console.log(listContainer)
+function render() {
+  //<li class="list-name">Grocery</li>
+  console.log(listContainer);
+  clearElement(listContainer);
+  lists.forEach((list) => {
+    const listElement = document.createElement("li");
+    listElement.classList.add("list-name");
+    listElement.innerText = list;
+    listContainer.appendChild(listElement)
+  });
+}
+
+function clearElement(element) {
+    while(element.firstChild){
+        element.removeChild(element.firstChild);
+    }
+}
+
+render();
+console.log(listContainer);
